0849a78996e8e49402a2c0dc9388879729f6b59a
MilicTG <milic.tg@gmail.com>
2021-04-17 15:48:05 +0200

    input field

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,55 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
- 
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
-  }
-  
+  MyAppState createState() => MyAppState();
+}
 
+class MyAppState extends State<MyApp> {
+  double _numberForm;
+
+  final List<String> _measures = [
+    'meters',
+    'kilometers',
+    'grams',
+    'kilograms',
+    'feet',
+    'miles',
+    'pounds (lbs)',
+    'ounces'
+  ];
+
+  @override
+  void initState() {
+    _numberForm = 0;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Unit converter",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Unit converter"),
+        ),
+        body: Center(
+            child: Column(
+          children: [
+            TextField(
+              onChanged: (text) {
+                var rv = double.tryParse(text);
+                if (rv != null) {
+                  setState(() {
+                    _numberForm = rv;
+                  });
+                }
+              },
+            ),
+            Text((_numberForm == null) ? "" : _numberForm.toString())
+          ],
+        )),
+      ),
+    );
+  }
 }
